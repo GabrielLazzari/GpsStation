dd11c4ee473f3f822de33c9c2cff535ca9e89ebb
GabrielLazzari <lazzarigcallegari@gmail.com>
2023-03-29 01:13:55 -0300

    Adicionado menu lateral

--- GpsStation/wwwroot/js/site.js
+++ GpsStation/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
 
-// Write your JavaScript code.
+menuToggle.onclick = function () {
+    navigation.classList.toggle('open');
+}
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active')
+    );
+    this.classList.add('active');
+}
+
+list.forEach((item) =>
+    item.addEventListener('click', activeLink)
+);
+
+document.addEventListener("mouseover", function (evt) {
+    if (evt.target.classList.contains("icon")) {
+        aux = evt.target.src
+        evt.target.src = evt.target.getAttribute("gif");
+        evt.target.setAttribute("gif", aux)
+    }
+});
+
+document.addEventListener("mouseout", function (evt) {
+    if (evt.target.classList.contains("icon")) {
+        aux = evt.target.src
+        evt.target.src = evt.target.getAttribute("gif");
+        evt.target.setAttribute("gif", aux)
+    }
+});
